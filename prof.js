2cb2cb2b31805723c1678506b070dc2c68f89e47
Mr-Anderson-git <mr.andersonsakk30@gmail.com>
2025-07-19 13:32:41 +0500

    Delete prof.js

--- prof.js
+++
@@ -1,8 +0,0 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    window.scrollTo({
-      top: section.offsetTop,
-      behavior: 'smooth'
-    });
-  }
-  
