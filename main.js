1422e1d4f4bbd9a1b0cc37647df0e35101832520
Mr-Anderson-git <mr.andersonsakk30@gmail.com>
2026-05-25 07:43:08 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+//1-misol
+let user = "Username"
+console.log(typeof user);
+//2-misol
+let age = 20
+console.log(age);
+console.log(age*5);
+console.log(typeof age);
+//3-misol
+const PI = 1234
+alert(PI)
+alert(typeof PI)
+
+
