66b7599d39de197599a2a0fd4e91963f88747634
Mr-Anderson-git <mr.andersonsakk30@gmail.com>
2026-05-25 08:50:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,110 +1,136 @@
 //1-misol
-let user = "Username"
+let user = "Username";
 console.log(typeof user);
+
 //2-misol
-let age = 20
+let age = 20;
 console.log(age);
-console.log(age*5);
+console.log(age * 5);
 console.log(typeof age);
+
 //3-misol
-const PI = 1234
-alert(PI)
-alert(typeof PI)
+const PI = 1234;
+alert(PI);
+alert(typeof PI);
+
 //4-misol
-let a = 5
-let b = 6
-let c 
-c = a+b
+let a = 5;
+let b = 6;
+let c; 
+c = a + b;
 console.log(c);
+
 //5-misol
-let str = "string"
-let num = 12
-let bool = True
-let undef 
-let n = null
-cosole.log(typeof str)
-cosole.log(typeof num)
-cosole.log(typeof bool)
-cosole.log(typeof undef)
-cosole.log(typeof n)
+let str = "string";
+let num = 12;
+let bool = true;
+let undef; 
+let n = null;
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof n);
+
 //6-misol
-var i = "Ism"
-i = "O'zgartirildi"
-console.log(i)
+var i = "Ism";
+i = "O'zgartirildi";
+console.log(i);
+
 //7-misol
-let tugilganYil = 1995
-console.log(2025-tugilganYil)
+let tugilganYil = 1995;
+console.log(2025 - tugilganYil);
+
 //8-misol
-let ism = "John"
-let familiya = "Doe"
-console.log(ism + " " + Doe)
+let ism = "John";
+let familiya = "Doe";
+console.log(ism + " " + familiya);
+
 //9-misol
-let ismm = "Farhod"
-let yosh = 20
-console.log('Salom, men $[ismm]man, yoshim $[yosh]')
+let ismm = "Farhod";
+let yosh = 20;
+console.log(`Salom, men ${ismm}man, yoshim ${yosh}`); 
+
 //10-misol 
-let u 
-console.log(u)
+let u; 
+console.log(u);
+
 //11-misol
-let nu = null
-console.log(null)
+let nu = null;
+console.log(nu);
+
 //12-misol
-let iism = "John Doe"
-let yyosh = 20
-let talabami = False
-console.log(iism)
-console.log(typeof iism)
-console.log(yyosh)
-console.log(typeof yyosh)
-console.log(talabami)
-console.log(typeof talabami)
+let iism = "John Doe";
+let yyosh = 20;
+let talabami = false;
+console.log(iism);
+console.log(typeof iism);
+console.log(yyosh);
+console.log(typeof yyosh);
+console.log(talabami);
+console.log(typeof talabami);
+
 //13-misol
-let text ="thisIsmMySiteListenLookAndListenAndLearn"
-console.log(text.length)
+let text = "thisIsmMySiteListenLookAndListenAndLearn";
+console.log(text.length);
+
 //14-misol
-let ages = "25"
+let ages = "25";
 console.log(Number(ages));
+
 //15-misol
-let yuz = 100
+let yuz = 100;
 console.log(String(yuz));
+
 //16-misol
-let son = 5
-let yangi
-yangi = son * 3
+let son = 5;
+let yangi;
+yangi = son * 3;
 console.log(yangi);
+
 //17-misol
-let bir = "Hello"
-let ikki = "World"
-console.log(bir +" "+ikki +"!")
+let bir = "Hello";
+let ikki = "World";
+console.log(bir + " " + ikki + "!");
+
 //18-misol
-let rang = "yashil"
-rang = "oq"
+let rang = "yashil";
+rang = "oq";
+
 //19-misol
-let ismi = "John"
-let familyasi = "Libert"
-let yoshi = 23
-console.log("We have new studen his name is " ismi + " " familyasi + " " + "he is same age " yoshi)
+let ismi = "John";
+let familyasi = "Libert";
+let yoshi = 23;
+
+console.log("We have new student his name is " + ismi + " " + familyasi + " he is same age " + yoshi);
+
 //20-misol
-let t = true
-let f = false
-console.log(t)
-console.log(f)
+let t = true;
+let f = false;
+console.log(t);
+console.log(f);
+
 //21-misol
 console.log(null == undefined);
+
 //22-misol
-console.log("I learn JS not because i love but i have to ")
+console.log("I learn JS not because i love but i have to ");
+
 //23-misol
-let sonn
-console.log(sonn++)
+let sonn = 1; 
+console.log(sonn++);
+
 //24-misol
-let name = "Albert" , aGe = 55 , job = "consultant";
-console.log('This is ${name} hi is ${age} years old he can help you he is ${job}');
+let name = "Albert", aGe = 55, job = "consultant";
+
+console.log(`This is ${name} hi is ${aGe} years old he can help you he is ${job}`);
+
 //25-misol
-let yoshingiz 
-let now
-yoshongiz = 1945
-now = 2026
-console.log(yoshingiz - now)
+let yoshingiz; 
+let now;
+yoshingiz = 1945; 
+now = 2026;
+console.log(now - yoshingiz);
 
 
 
