acc9781f9be21ea3edd7d5e6ed183dd501e73f1d
Mr-Anderson-git <mr.andersonsakk30@gmail.com>
2026-05-25 08:37:18 +0500

    Update main.js

--- main.js
+++ main.js
@@ -10,5 +10,106 @@ console.log(typeof age);
 const PI = 1234
 alert(PI)
 alert(typeof PI)
+//4-misol
+let a = 5
+let b = 6
+let c 
+c = a+b
+console.log(c);
+//5-misol
+let str = "string"
+let num = 12
+let bool = True
+let undef 
+let n = null
+cosole.log(typeof str)
+cosole.log(typeof num)
+cosole.log(typeof bool)
+cosole.log(typeof undef)
+cosole.log(typeof n)
+//6-misol
+var i = "Ism"
+i = "O'zgartirildi"
+console.log(i)
+//7-misol
+let tugilganYil = 1995
+console.log(2025-tugilganYil)
+//8-misol
+let ism = "John"
+let familiya = "Doe"
+console.log(ism + " " + Doe)
+//9-misol
+let ismm = "Farhod"
+let yosh = 20
+console.log('Salom, men $[ismm]man, yoshim $[yosh]')
+//10-misol 
+let u 
+console.log(u)
+//11-misol
+let nu = null
+console.log(null)
+//12-misol
+let iism = "John Doe"
+let yyosh = 20
+let talabami = False
+console.log(iism)
+console.log(typeof iism)
+console.log(yyosh)
+console.log(typeof yyosh)
+console.log(talabami)
+console.log(typeof talabami)
+//13-misol
+let text ="thisIsmMySiteListenLookAndListenAndLearn"
+console.log(text.length)
+//14-misol
+let ages = "25"
+console.log(Number(ages));
+//15-misol
+let yuz = 100
+console.log(String(yuz));
+//16-misol
+let son = 5
+let yangi
+yangi = son * 3
+console.log(yangi);
+//17-misol
+let bir = "Hello"
+let ikki = "World"
+console.log(bir +" "+ikki +"!")
+//18-misol
+let rang = "yashil"
+rang = "oq"
+//19-misol
+let ismi = "John"
+let familyasi = "Libert"
+let yoshi = 23
+console.log("We have new studen his name is " ismi + " " familyasi + " " + "he is same age " yoshi)
+//20-misol
+let t = true
+let f = false
+console.log(t)
+console.log(f)
+//21-misol
+console.log(null == undefined);
+//22-misol
+console.log("I learn JS not because i love but i have to ")
+//23-misol
+let sonn
+console.log(sonn++)
+//24-misol
+let name = "Albert" , aGe = 55 , job = "consultant";
+console.log('This is ${name} hi is ${age} years old he can help you he is ${job}');
+//25-misol
+let yoshingiz 
+let now
+yoshongiz = 1945
+now = 2026
+console.log(yoshingiz - now)
+
+
+
+
+
+
 
 
